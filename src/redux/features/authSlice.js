0b437436fef257a591fdffc70b794f933301e21b
Musfiq <musfiqrumel@gmail.com>
2022-11-09 03:46:05 +0600

    register working perfectly

--- src/redux/features/authSlice.js
+++ src/redux/features/authSlice.js
@@ -18,16 +18,16 @@ export const login = createAsyncThunk(
 export const register = createAsyncThunk(
     "auth/register",
     async ({ formValue, navigate, toast }, { rejectWithValue }) => {
-      try {
-        const response = await api.signUp(formValue);
-        toast.success("Register Successfully");
-        navigate("/");
-        return response.data;
-      } catch (err) {
-        return rejectWithValue(err.response.data);
-      }
+        try {
+            const response = await api.signUp(formValue);
+            toast.success("Register Successfully");
+            navigate("/");
+            return response.data;
+        } catch (err) {
+            return rejectWithValue(err.response.data);
+        }
     }
-  );
+);
 
 const authSlice = createSlice({
     name: "auth",
@@ -38,16 +38,28 @@ const authSlice = createSlice({
     },
     extraReducers: {
         [login.pending]: (state, action) => {
-          state.loading = true;
+            state.loading = true;
         },
         [login.fulfilled]: (state, action) => {
-          state.loading = false;
-          localStorage.setItem("profile", JSON.stringify({ ...action.payload }));
-          state.user = action.payload;
+            state.loading = false;
+            localStorage.setItem("profile", JSON.stringify({ ...action.payload }));
+            state.user = action.payload;
         },
         [login.rejected]: (state, action) => {
-          state.loading = false;
-          state.error = action.payload.message;
+            state.loading = false;
+            state.error = action.payload.message;
+        },
+        [register.pending]: (state, action) => {
+            state.loading = true;
+        },
+        [register.fulfilled]: (state, action) => {
+            state.loading = false;
+            localStorage.setItem("profile", JSON.stringify({ ...action.payload }));
+            state.user = action.payload;
+        },
+        [register.rejected]: (state, action) => {
+            state.loading = false;
+            state.error = action.payload.message;
         },
     },
 });
