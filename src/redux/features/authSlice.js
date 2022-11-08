132da55a5355bb694169f09d5a73f5f49bc05a45
Musfiq <musfiqrumel@gmail.com>
2022-11-09 03:36:59 +0600

    Register page almost done

--- src/redux/features/authSlice.js
+++ src/redux/features/authSlice.js
@@ -15,6 +15,20 @@ export const login = createAsyncThunk(
     }
 );
 
+export const register = createAsyncThunk(
+    "auth/register",
+    async ({ formValue, navigate, toast }, { rejectWithValue }) => {
+      try {
+        const response = await api.signUp(formValue);
+        toast.success("Register Successfully");
+        navigate("/");
+        return response.data;
+      } catch (err) {
+        return rejectWithValue(err.response.data);
+      }
+    }
+  );
+
 const authSlice = createSlice({
     name: "auth",
     initialState: {
